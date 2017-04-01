31cbf42d2235b70ecf66280c7eaba81578cb79e1
vmandic <mandic.vedran@gmail.com>
2017-04-01 10:14:05 +0200

    redux form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/index";
+import { Link } from "react-router";
 
-export default () => {
-  return (
-    <div>List of blog posts.</div>
-  );
+class PostsIndex extends Component {
+  componentWillMount() {
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a post
+          </Link>
+        </div>
+        posts...
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
